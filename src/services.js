fadd3be0b8f2d59d22ba0ccf15ec07bd78bbf76e
Layton Whiteley <layton.whiteley@gmail.com>
2016-07-18 18:36:53 -0400

    chore: update passing options

--- src/services.js
+++ src/services.js
@@ -7,23 +7,28 @@ var definitions = require('./definitions'),
       extensions: 'ext'
     };
 
-service.list = function(category, options){
+service.list = function(options){
   var list = [],
-      _options = {
-        list: 'formats'
+      doptions = {
+        category: null,
+        list: 'extensions'
       };
-  var doptions = _.merge(_options, options);
+  if( !_.isPlainObject(options) ){
+    doptions.category = options;
+  }else{
+      doptions = _.merge(doptions, options);
+  }
 
   list = _.map(definitions, function(definition){
     if(_.isPlainObject(definition) ){
-        if(_.isString(category) &&
+        if(_.isString(doptions.category) &&
             _.isString(definition.category) &&
-            definition.category.toLowerCase() === category.toLowerCase()
+            definition.category.toLowerCase() === doptions.category.toLowerCase()
           ){
           return definition[propMap[doptions.list]].toLowerCase();
         }
 
-        if( !_.isString(category) ){
+        if( !_.isString(doptions.category) ){
           return definition[propMap[doptions.list]].toLowerCase();
         }
     }
