8d55451bec5cb1aea19b8e8e6b71d0606ae39dc5
Layton Whiteley <layton.whiteley@gmail.com>
2016-07-18 20:57:18 -0400

    0.0.4

--- src/definitions/gif.def.js
+++ src/definitions/gif.def.js
@@ -16,7 +16,7 @@ var definitions = [
      container: 'HTML',
      videoCodingFormats: 'Any',
      audioCodecs: 'none',
-     category: 'video',
+     category: 'video,image',
      notes: 'Not standardized, and not a real video file in the classical meaning since it merely references the real video file (e.g. a .webm file), which has to exist separately elsewhere. A .gifv "file" is simply a HTML webpage which includes a HTML5 video tag, where the video has no sound. As there were large communities online which create art using the medium of short soundless videos in GIF format, GIFV was created as a functionally similar replacement with vastly smaller filesizes than the inefficient GIF format.'
    },
 
