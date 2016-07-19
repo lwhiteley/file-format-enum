e9332924a9c89ed24831c97c135a998a0afa168a
Layton Whiteley <layton.whiteley@gmail.com>
2016-07-18 20:46:07 -0400

    chore: add notes to readme

---
+++ src/definitions/audio.js
@@ -0,0 +1,21 @@
+var _ = require('lodash');
+var commonProps = {
+  category: 'audio'
+}
+
+var definitions = [
+  _.merge({
+     ext: '.mp3',
+    name: 'MP3',
+    notes: 'MPEG Layer III Audio. Is the most common sound file format used today.'
+  },commonProps),
+
+  _.merge({
+     ext: '.3gp',
+    name: '3GP',
+    notes: 'Multimedia container format can contain proprietary formats as AMR, AMR-WB or AMR-WB+, but also some open formats'
+  },commonProps)
+];
+
+
+module.exports = definitions;
