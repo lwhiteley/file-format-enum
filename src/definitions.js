84d035853e77a7a7962a3829dc10cbeedb80d834
Layton Whiteley <layton.whiteley@gmail.com>
2016-07-20 11:34:50 -0400

    0.0.9

--- src/definitions.js
+++ src/definitions.js
@@ -1,18 +1,29 @@
-module.exports = function(root){
-  var glob = require('glob'),
-      _ = require('lodash'),
-      PATH = require('path'),
-      definitionsFiles = glob.sync('./src/definitions/**/*.js', {root: root});
+module.exports = function(root) {
+    var glob = require('glob'),
+        _ = require('lodash'),
+        PATH = require('path');
+    //definitionsFiles = glob.sync('./src/definitions/**/*.js', {root: root});
 
-  var compiledDefs = [];
-  
-  _.forEach(definitionsFiles, function(defFile){
-    var def = require(PATH.resolve(root, defFile));
-    if(_.isArray(def)){
-      compiledDefs = _.union(compiledDefs, def);
-    }
-  })
+    var definitionsFiles = [
+        './src/definitions/3gp.def.js',
+        './src/definitions/audio.def.js',
+        './src/definitions/flash.def.js',
+        './src/definitions/gif.def.js',
+        './src/definitions/mp4.def.js',
+        './src/definitions/mpeg1.def.js',
+        './src/definitions/mpeg2.def.js',
+        './src/definitions/video.def.js'
+    ];
 
-  return compiledDefs;
+    var compiledDefs = [];
+
+    _.forEach(definitionsFiles, function(defFile) {
+        var def = require(PATH.resolve(root, defFile));
+        if (_.isArray(def)) {
+            compiledDefs = _.union(compiledDefs, def);
+        }
+    })
+
+    return compiledDefs;
 
 };
