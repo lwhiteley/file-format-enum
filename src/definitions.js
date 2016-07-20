3e6d3ba2b820748b664cea3cb47c10cc1827900c
Layton Whiteley <layton.whiteley@gmail.com>
2016-07-20 12:06:58 -0400

    0.0.11

--- src/definitions.js
+++ src/definitions.js
@@ -2,23 +2,12 @@ module.exports = function(root) {
     var glob = require('glob'),
         _ = require('lodash'),
         PATH = require('path');
-    //definitionsFiles = glob.sync('./src/definitions/**/*.js', {root: root});
-
-    var definitionsFiles = [
-        './definitions/3gp.def.js',
-        './definitions/audio.def.js',
-        './definitions/flash.def.js',
-        './definitions/gif.def.js',
-        './definitions/mp4.def.js',
-        './definitions/mpeg1.def.js',
-        './definitions/mpeg2.def.js',
-        './definitions/video.def.js'
-    ];
+    _definitionFiles = glob.sync(root+'/src/definitions/**/*.js');
 
     var compiledDefs = [];
 
-    _.forEach(definitionsFiles, function(defFile) {
-        var def = require(defFile);
+    _.forEach(_definitionFiles, function(defFile, i) {
+        var def = require((defFile));
         if (_.isArray(def)) {
             compiledDefs = _.union(compiledDefs, def);
         }
